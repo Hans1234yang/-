770424fe0549c9519e3096a7c83eb7a21ef2938c
hans <545872067@qq.com>
2018-08-24 17:22:43 +0800

    增加util

--- WebApplication1/Controllers/StudentController.cs
+++ WebApplication1/Controllers/StudentController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+using Enterprise.BackServer.Utility;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -19,11 +21,32 @@ namespace WebApplication1.Controllers
         // GET: Student
         public ActionResult Index()
         {
-            var query = _studentServer.LoadAll(s=>s.stuname=="hans");
+            var query = _studentServer.LoadAll(s => s.stuname == "hans");
             //排序
             query = query.OrderBy(s => s.stuid);
+
+            var result = query.ToList();
             //分页
-            Util.PagedList<studnet> list = new Util.PagedList<studnet>(query,pageIndex,pageIndex);
+            var jsonData = JsonConverter.Serialize(new PageLayUi(query.ToList().Count, result, ""), true);
+            return Content(jsonData, "application/json");
         }
+    }
+
+    /// <summary>
+    /// layui分页处理逻辑类
+    /// </summary>
+    public class PageLayUi
+    {
+        public PageLayUi(int count, object data, string msg = "")
+        {
+            code = 0;
+            this.msg = msg;
+            this.count = count;
+            this.data = data;
+        }
+        public int code { get; private set; }
+        public string msg { get; private set; }
+        public int count { get; private set; }
+        public object data { get; private set; }
     }
 }
